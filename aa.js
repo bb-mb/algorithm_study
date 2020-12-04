2744ab3097f2ef66677ab542760d2d0db2f5a4cf
soso01 <6125024@gmail.com>
2020-12-04 15:29:00 +0900

    12 04

--- aa.js
+++ aa.js
@@ -1,34 +1,7 @@
-const a = [
-  [
-    [
-      [[], []],
-      [[], []],
-    ],
-    [
-      [[], []],
-      [[], [260]],
-    ],
-  ],
-  [
-    [
-      [[], []],
-      [[], []],
-    ],
-    [
-      [[80], [150]],
-      [[], []],
-    ],
-  ],
-  [
-    [
-      [[], []],
-      [[150, 150, 150, 210], []],
-    ],
-    [
-      [[], []],
-      [[50], []],
-    ],
-  ],
-]
-
-console.log(a[2][0][1][0])
+function Func() {}
+Func.prototype.num = 2;
+var a = new Func();
+console.log(a.num); // 2
+a.num = 1;
+console.log(a.num); // 1
+console.log(Func.prototype.num) // 2
